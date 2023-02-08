82777b1b772637f49bb46acafeb9149a677f94dc
Pranam451 <ppranam451@gmail.com>
2023-02-08 21:08:51 +0530

    images uploaded

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,9 +2,12 @@ import React from "react";
 
 export const Home = () => {
   return (
-    <div className=" w-full flex sm:flex-row flex-col home-main">
+    <div className=" w-full flex sm:flex-row flex-col gap-5 home-main">
       <div className="p-5">
-        <img src="" alt="" />
+        <img
+          src="https://github.com/Pranam451/conilado/blob/master/src/Assets/istockphoto-1211524213-170667a.png?raw=true"
+          alt=""
+        />
       </div>
     </div>
   );
